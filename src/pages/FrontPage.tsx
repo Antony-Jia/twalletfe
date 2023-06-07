2d1031bb2c04755209b13b3e4bd62369fd73b9df
Antony-Jia <527223709@qq.com>
2023-06-07 17:06:57 +0800

    add multi fetch write

--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -13,6 +13,19 @@ function FrontPage() {
   // const [data, setData] = useState(null);
   // const [loading, setLoading] = useState(true);
 
+
+  const findAnyName = async () => {
+    const urls = ['https://randomuser.me/api/', 'https://randomuser.me/api/'];
+    try {
+      let res = await Promise.all(urls.map(e => fetch(e)))
+      let resJson = await Promise.all(res.map(e => e.json()))
+      resJson = resJson.map(e => e.results[0].name.first)
+      console.log(resJson)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   // useEffect(() => {
   //   const fetchData = async () => {
   //     setLoading(true);
